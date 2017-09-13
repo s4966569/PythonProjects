d3b836fb016c43159394b1d985bcf4c5d134361d
sunpeng <sunpeng@yanxiu.com>
2017-09-13 13:10:39 +0800

    commit

--- webcrawler/PostInfo.py
+++ webcrawler/PostInfo.py
@@ -2,8 +2,4 @@ class PostInfo:
     def __init__(self):
         self.total_page = None
         self.total_reply = None
-<<<<<<< HEAD
         self.title = None
-=======
-        self.title = None
->>>>>>> 9f7a4d5ee914a9324ef3c2181c525cc2c10582cb
